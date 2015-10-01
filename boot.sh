67eab1962c550a44530288bd717a3313eb1414d6
Naveed Masaud Butt <naveed.butt@metadesign.eu>
2015-10-01 16:39:27 +0500

    Modify boot.sh

--- boot.sh
+++ boot.sh
@@ -5,47 +5,50 @@ source ./utils.sh
 welcome=`cat ascii`
 echo "$welcome"
 
-render_message 1 "heading" "This will start the initial process for setting up your project, Are you sure you want to continue? (y/n)"
+render_message 1 "heading" "This will start the initial process for setting up your project, Are you sure you want to proceed? (y/n)"
 read answer
 
 if echo "$answer" | grep -iq "^y" ;then
   
-    render_message 1 "input" "1. Project directory name without any spaces: (example: motosport):"
-    read answer
-    directory_name=${answer}
-    #echo $directory_name
-
     current_path=$(pwd)
     #echo $current_path
         
-    render_message 1 "input" "2. Git repository (example: git@git.company.com:project/name):"
+    render_message 1 "input" "1. Git repository (example: git@git.company.com:project/name):"
     read answer
     git_repo=${answer}
     #echo $git_repo
 
-    render_message 1 "input" "3. MySQL database name:"
+    render_message 1 "input" "2. MySQL database name: (same as you entered in config-custom.yaml file)"
     read answer
     database_name=${answer}
     #echo $database_name
 
-    render_message 1 "input" "4. MySQL user (root) password:"
+    render_message 1 "input" "3. MySQL user (root) password: (same as you entered in config-custom.yaml file)"
     read answer
     db_root_password=${answer}
     #echo $db_root_password    
 
-    full_path=$current_path/$directory_name
+    full_path=$current_path
     #echo $full_path
     
+    # Temporary git directory
+    tmp_git_dir=$current_path/temp
+    
     render_message 0 "status" "Initializing Git Cloning..."
-    git clone $git_repo $full_path
+    git clone $git_repo $tmp_git_dir
 
     render_message 0 "status" "Initializing Submodules..."
-    cd $full_path
+    cd $tmp_git_dir
     git checkout develop
     git submodule update --init
-    cd $full_path/application
+    cd $tmp_git_dir/application
     git checkout develop
     git submodule update --init
+
+    # Moving back all files from that 'temp' folder to our main working folder    
+    shopt -s dotglob nullglob    # To make sure hidden files are included as well
+    mv $tmp_git_dir/* $current_path/
+    rm -rf $tmp_git_dir # Remove temporary folder and files in it
 
     render_message 0 "status" "Creating and assigning permissions to directories..."
     cd $full_path
@@ -74,11 +77,10 @@ if echo "$answer" | grep -iq "^y" ;then
     render_message 0 "status" "Initializing Database Migrations..."
     ./yiic deploy migrate
 
-    render_message 0 "status" "Initializing Sass Compilation, Merging Dir, Flushing Caches..."
-    ./yiic deploy sass merge-web gen-js mini-js flush-cache
+    render_message 0 "status" "Initializing Sass Compilation, Merging Directories"
+    ./yiic deploy sass merge-web gen-js mini-js
 
     render_message 0 "heading" "Setup is now completed"
-
 else
     render_message 0 "heading" "GoodBye!"
 fi
